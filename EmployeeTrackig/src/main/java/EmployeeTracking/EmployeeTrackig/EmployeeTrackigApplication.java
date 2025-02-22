a61d33052765918691ffe9efaa7511eef12c336b
KIRANME78 <KIRANME438@GMAIL.COM>
2025-02-21 23:34:07 -0800

    In it

---
+++ EmployeeTrackig/src/main/java/EmployeeTracking/EmployeeTrackig/EmployeeTrackigApplication.java
@@ -0,0 +1,13 @@
+package EmployeeTracking.EmployeeTrackig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTrackigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeTrackigApplication.class, args);
+	}
+
+}
